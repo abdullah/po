bbea65c5ea027b41db5491fa49ea1adad5d77adb
Abdullah Mara <zull.mara00@gmail.com>
2017-06-22 10:49:15 +0300

    Change sound volume

--- assets/po.js
+++ assets/po.js
@@ -8,6 +8,7 @@ function setValue(el, val) {
 }
 
 var Synth = new AudioSynth();
+Synth.setVolume(0.1); // set volume to 100%
 var instrument = {
   piano: Synth.createInstrument('piano'),
   organ: Synth.createInstrument('organ'),
@@ -38,12 +39,7 @@ function Po(options) {
 
   this.LString = '';
 
-  setValue(
-    '#rules',
-    this.rules
-      .map(r => `${r[0]} = ${r[1]}`)
-      .join(',\n')
-  );
+  setValue('#rules', this.rules.map(r => `${r[0]} = ${r[1]}`).join(',\n'));
 
   setValue('#iteration', this.iteration);
   setValue('#axiom', this.axiom);
@@ -52,34 +48,34 @@ function Po(options) {
   setValue('#duration', this.duration);
 }
 
-Po.prototype.getValuesFromDocument = function () {
+Po.prototype.getValuesFromDocument = function() {
   this.iteration = getValue('#iteration');
   this.axiom = getValue('#axiom');
   this.octave = getValue('#octave');
   this.duration = getValue('#duration');
   this.defaultInstrument = getValue('#instrument');
 
-  this.rules = getValue('#rules').split(',').map(r => r.replace(/\s/g, '').split('='));
+  this.rules = getValue('#rules')
+    .split(',')
+    .map(r => r.replace(/\s/g, '').split('='));
 };
 
-Po.prototype.init = function () {
+Po.prototype.init = function() {
   this.getValuesFromDocument();
   this.generateLString(this.axiom);
 };
 
-Po.prototype.generateLString = function (str) {
+Po.prototype.generateLString = function(str) {
   this.LString = str;
   var tmp = '';
   var _rules = this.rules.concat(this.constant);
-  Array.from(str).map(
-    (c) => {
-      _rules.map((rule) => {
-        if (rule[0] == c) {
-          tmp += c.replace(rule[0], rule[1]);
-        }
-      });
-    }
-  );
+  Array.from(str).map(c => {
+    _rules.map(rule => {
+      if (rule[0] == c) {
+        tmp += c.replace(rule[0], rule[1]);
+      }
+    });
+  });
 
   if (this.iteration) {
     this.iteration--;
@@ -89,73 +85,72 @@ Po.prototype.generateLString = function (str) {
 
 var interval = null;
 
-Po.prototype.play = function () {
+Po.prototype.play = function() {
   console.log('Result : ', this.LString);
 
   var notes = Array.from(this.LString);
   var size = notes.length;
   var duration = this.duration;
   var d = 100;
-  interval = setInterval(
-    () => {
-      var index = notes.length - size;
-      var note = notes[index];
-      var nextNote = notes[index + 1];
-      // this.draw()
-      if (size > 0) {
-        try {
-          switch (note) {
-            case '{':
-              // instrument.organ.play(nextNote,this.octave,duration)
-              break;
-            case '}':
-              // instrument.acoustic.play(nextNote,this.octave,duration)
-              break;
-            case '[':
-              // instrument.edm.play(nextNote,this.octave,duration)
-              break;
-            case ']':
-              // instrument.piano.play(nextNote,this.octave,duration)
-              break;
-            case '+':
-              // instrument[this.defaultInstrument].play(nextNote,this.octave+1,duration)
-              break;
-            case '-':
-              // instrument[this.defaultInstrument].play(nextNote,this.octave-1,duration)
-              // context.rotate(Math.PI / 2);
-              break;
-            default:
-              // context.rotate(Math.PI / 1);
-              const audio = document.querySelector(`audio[data-key="${note}"]`);
-              audio.currentTime = 0;
-              audio.play();
+  interval = setInterval(() => {
+    var index = notes.length - size;
+    var note = notes[index];
+    var nextNote = notes[index + 1];
+    // this.draw()
+    if (size > 0) {
+      try {
+        switch (note) {
+          case '{':
+            // instrument.organ.play(nextNote,this.octave,duration)
+            break;
+          case '}':
+            // instrument.acoustic.play(nextNote,this.octave,duration)
+            break;
+          case '[':
+            // instrument.edm.play(nextNote,this.octave,duration)
+            break;
+          case ']':
+            // instrument.piano.play(nextNote,this.octave,duration)
+            break;
+          case '+':
+            // instrument[this.defaultInstrument].play(nextNote,this.octave+1,duration)
+            break;
+          case '-':
+            // instrument[this.defaultInstrument].play(nextNote,this.octave-1,duration)
+            // context.rotate(Math.PI / 2);
+            break;
+          default:
+            // context.rotate(Math.PI / 1);
+            const audio = document.querySelector(`audio[data-key="${note}"]`);
+            audio.currentTime = 0;
+            audio.volume = 0.08;
 
-              instrument[this.defaultInstrument].play(
-                note,
-                this.octave,
-                duration
-              );
-              break;
-          }
-        } catch (e) {
-          // statements
-          console.log(e, note);
+            audio.play();
+
+            instrument[this.defaultInstrument].play(
+              note,
+              this.octave,
+              duration
+            );
+            break;
         }
-      } else {
-        clearInterval(interval);
+      } catch (e) {
+        // statements
+        console.log(e, note);
       }
+    } else {
+      clearInterval(interval);
+    }
 
-      size--;
-    },
-    duration * 100
-  );
+    size--;
+  }, duration * 100);
 };
 
-Po.prototype.stop = function () {
+Po.prototype.stop = function() {
   clearInterval(interval);
 };
 
-Po.prototype.start = function () {
+Po.prototype.start = function() {
   this.stop();
   this.init();
   this.play();
